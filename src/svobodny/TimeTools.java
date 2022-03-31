d63b910ecaf037aa1ee14530418335b6b3f979ef
Hynek Vaclav Svobodny <hynek.vaclav.svobodny@tul.cz>
2022-03-31 10:07:00 +0200

    Added svobodny package

---
+++ src/svobodny/TimeTools.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package svobodny;
+
+/**
+ *
+ * @author hynek.vaclav.svobodny
+ */
+public class TimeTools {
+    private TimeTools() {}
+}
---
+++ src/svobodny/Zavodnik.java
@@ -0,0 +1,18 @@
+package svobodny;
+
+/**
+ *
+ * @author hynek.vaclav.svobodny
+ */
+public class Zavodnik {
+    // Instance variables
+    private String name;
+    private String surname;
+    private String birthYear;
+    private Sex sex;
+    private String club;
+    private int startNumber;
+    private int startTime;
+    private int finishTime;
+    
+}
