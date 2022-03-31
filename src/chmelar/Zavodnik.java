d24c809e767246c3493456dcb149f81e541f7351
Tomas Chmelar <tomas.chmelar@tul.cz>
2022-03-31 08:53:21 +0200

    zacatek

---
+++ src/chmelar/Zavodnik.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chmelar;
+
+/**
+ *
+ * @author tomas.chmelar
+ */
+public class Zavodnik {
+    private String jmeno;
+    private String prijmeni;
+}
