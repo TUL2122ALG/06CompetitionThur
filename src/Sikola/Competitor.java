23bf193631c230a842714d21ff71d95ff1c9b47b
Jakub Sikola <jakub.sikola@tul.cz>
2022-04-01 11:02:34 +0200

    Added competitor, competetion and timeTools classes

--- src/Sikola/Competitor.java
+++ src/Sikola/Competitor.java
@@ -9,5 +9,52 @@ package Sikola;
  * @author kubas
  */
 public class Competitor {
+    private String name;
+    private char gender;
+    private int yearOfBirth;
+    private int timeStart;
+    private int timeEnd;
+    private int timeOfRace;
+    private int regNum;
     
+
+    private Competitor(String name, char gender, int yearOfBirth) {
+        this.name = name;
+        this.gender = gender;
+        this.yearOfBirth = yearOfBirth;
+    }
+    
+    public static Competitor getInstanceOf(String name, char gender, int yearOfBirth){
+        return new Competitor(name,gender, yearOfBirth);
+    }
+
+    public void setRegNum(int regNum) {
+        this.regNum = regNum;
+    }
+    
+    public int getTimeOfRace(){
+        return timeTools.getCompetitorTime(this.timeStart, this.timeEnd);  
+    }
+    
+    public String getTimeOfRaceStr(){
+        return timeTools.timeToStr(this.timeOfRace);
+    }
+    
+    public void seTimeStart(int timeStart){
+        this.timeStart = timeStart;
+    }
+    
+    public void setTimeEnd(int timeEnd){
+        this.timeEnd = timeEnd;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    
+    
+    
+    
+            
 }
